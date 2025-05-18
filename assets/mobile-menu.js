ee72edc11de241930191ab009a126e5c50a19dda
supermediapk <yahyaaamir474@gmail.com>
2025-05-18 13:56:58 +0500

    Add files via upload

---
+++ assets/mobile-menu.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu-button').addEventListener('click', function () {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+});
